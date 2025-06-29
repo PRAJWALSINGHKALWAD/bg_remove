69b07ac8720a23aec09ce78352bf31cd8e62542d
PRAJWALSINGHKALWAD <prajwalsinghkalwad8@gmail.com>
2025-06-29 11:07:57 +0530

    so many removed

--- run.ps1
+++
@@ -1,8 +0,0 @@
-# PowerShell script to run background removal with virtual environment
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$venvPath = Join-Path $scriptDir "venv\Scripts\Activate.ps1"
-$pythonScript = Join-Path $scriptDir "bg-remove.py"
-
-# Activate virtual environment and run the script
-& $venvPath
-python $pythonScript $args
--- run_removebg.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Remove.bg Quality Background Removal System
-call "%~dp0venv\Scripts\activate"
-python "%~dp0removebg_quality.py" %*
